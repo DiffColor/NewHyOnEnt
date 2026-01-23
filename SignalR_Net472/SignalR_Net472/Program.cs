7dcda58d8993f2e1509c0090a9a6ebbcac776afb
jazzlife <jazzlife.dev@gmail.com>
2026-01-23 18:57:28 +0900

    signalr 분리

--- SignalR_Net472/SignalR_Net472/Program.cs
+++ SignalR_Net472/SignalR_Net472/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SignalRNet472
 {
@@ -11,23 +12,19 @@ namespace SignalRNet472
         {
             Console.CancelKeyPress += OnCancelKeyPress;
             AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
 
             try
             {
-                SignalRServerHost.Start();
-                Console.WriteLine("SignalR_Net472 started. Press Ctrl+C to stop.");
-                ShutdownEvent.WaitOne();
+                RunWithRestartLoop();
                 return 0;
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine($"SignalR_Net472 failed to start: {ex}");
-                Logger.WriteErrorLog($"SignalR_Net472 failed to start: {ex}", Logger.GetLogFileName());
+                Console.Error.WriteLine($"SignalR_Net472 fatal error: {ex}");
+                Logger.WriteErrorLog($"SignalR_Net472 fatal error: {ex}", Logger.GetLogFileName());
                 return 1;
-            }
-            finally
-            {
-                SignalRServerHost.Stop();
             }
         }
 
@@ -40,6 +37,46 @@ namespace SignalRNet472
         private static void OnProcessExit(object sender, EventArgs e)
         {
             ShutdownEvent.Set();
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Logger.WriteErrorLog($"Unhandled exception: {e.ExceptionObject}", Logger.GetLogFileName());
+        }
+
+        private static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Logger.WriteErrorLog($"Unobserved task exception: {e.Exception}", Logger.GetLogFileName());
+            e.SetObserved();
+        }
+
+        private static void RunWithRestartLoop()
+        {
+            while (!ShutdownEvent.WaitOne(0))
+            {
+                try
+                {
+                    SignalRServerHost.Start();
+                    Console.WriteLine("SignalR_Net472 started. Press Ctrl+C to stop.");
+                    while (!ShutdownEvent.WaitOne(1000))
+                    {
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"SignalR_Net472 error: {ex}");
+                    Logger.WriteErrorLog($"SignalR_Net472 error: {ex}", Logger.GetLogFileName());
+                }
+                finally
+                {
+                    SignalRServerHost.Stop();
+                }
+
+                if (!ShutdownEvent.WaitOne(0))
+                {
+                    Thread.Sleep(5000);
+                }
+            }
         }
     }
 }
