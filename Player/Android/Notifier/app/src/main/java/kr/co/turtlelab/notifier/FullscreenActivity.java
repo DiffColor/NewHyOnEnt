db5f6647f68d478c093a5e413e2638fc733e312c
jazzlife <jazzlife.dev@gmail.com>
2026-03-09 01:23:01 +0900

    feat: add Android notifier project

---
+++ Player/Android/Notifier/app/src/main/java/kr/co/turtlelab/notifier/FullscreenActivity.java
@@ -0,0 +1,151 @@
+package kr.co.turtlelab.notifier;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends Activity {
+    public static final String ACTION_SHOW_MESSAGE = "kr.co.turtlelab.notifier.msg";
+    public static final String ACTION_FINISH = "kr.co.turtlelab.notifier.finish";
+    public static final String EXTRA_MESSAGE = "msg";
+
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private GifMovieView mGifView;
+    private TextView mTextView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mGifView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+
+    public static FullscreenActivity act = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        act = this;
+
+        mGifView = (GifMovieView) findViewById(R.id.gifView);
+        mGifView.setMovieResource(R.drawable.init);
+
+        mTextView = (TextView) findViewById(R.id.msgView);
+        applyIntent(getIntent());
+    }
+
+    public void setMsg(String msg) {
+        mTextView.setText(msg);
+    }
+
+    public void handleAction(String action, String msg) {
+        if (ACTION_FINISH.equals(action)) {
+            mHideHandler.removeCallbacks(mHideRunnable);
+            mHideHandler.removeCallbacks(mHidePart2Runnable);
+            closeActivity();
+            return;
+        }
+
+        if (ACTION_SHOW_MESSAGE.equals(action)) {
+            setMsg(msg == null ? "" : msg);
+            delayedHide(100);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        applyIntent(intent);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void hide() {
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (act == this) {
+            act = null;
+        }
+        super.onDestroy();
+    }
+
+    private void applyIntent(Intent intent) {
+        if (intent == null) {
+            return;
+        }
+        handleAction(intent.getAction(), intent.getStringExtra(EXTRA_MESSAGE));
+    }
+
+    private void closeActivity() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAndRemoveTask();
+            return;
+        }
+        finish();
+    }
+}
